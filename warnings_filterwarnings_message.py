ceaff16ee75f2f9b0e69d6b902976d6523fce485
10694 <1069498513@qq.com>
2024-12-21 20:38:03 +0800

    加个实例

--- warnings_filterwarnings_message.py
+++ warnings_filterwarnings_message.py
@@ -4,3 +4,14 @@ warnings.filterwarnings('ignore', '.*do not.*',)
 
 warnings.warn('Show this message')
 warnings.warn('Do not show this message')
+# 写个例子，看看效果：
+# Example of filtering warnings based on message content
+
+# Filter warnings that contain the phrase 'ignore this'
+warnings.filterwarnings('ignore', '.*ignore this.*')
+
+# This warning will be shown
+warnings.warn('This is a warning message')
+
+# This warning will be ignored
+warnings.warn('Please ignore this warning message')
